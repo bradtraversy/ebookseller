848ba87995de53a014a6db8813a34b6bf961f64b
Brad Traversy <me@bradtraversy.com>
2017-10-10 14:33:09 -0400

    fixed keys file

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-if(process.env.NOD_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
   module.exports = require('./keys_prod');
 } else {
   module.exports = require('./keys_dev');
